e0ee7983f149a6921aa86ba8403505d9bcc79c73
CyberCatStudios <gwaine777@gmail.com>
2025-04-04 09:09:42 +0400

    Made a few quick code updates to justify pushing

--- mserver.py
+++ mserver.py
@@ -1,7 +1,2 @@
 def mbopServerFunc():
-    version = "0.0.1"
-    commands =["help", "list", "quit"]
-    players = []
-    index = 0
-    index += 1
-    players.append(str(index))
+    print("Server started!")
